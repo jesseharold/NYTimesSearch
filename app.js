2897f1093acaa65e38fa5acf42828113b89e0246
jesseharold <jesseharold@gmail.com>
2016-10-31 13:33:06 -0700

    added clear button, fixed start and end date formatting

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 // Built by LucyBot. www.lucybot.com
 	
 $("document").ready(function(){
+	//event listeners
 	$("#submit").click(onSubmit);
+	$("#clear").click(function(){
+		$("#searchTerm").val("");
+		$("#numRecords").val("5");
+		$("#startYear").val("");
+		$("#endYear").val("");
+	});
 });
 function onSubmit(){	
 	var searchURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 	var key = "ca789060234c4688a4bc3a324e26de6c";
 	var keyword = $("#searchTerm").val();
-	var startDate = $("#startYear").val(); //Format YYYYMMDD
+	// should validate year format here, or use a date picker
+	var startDate = $("#startYear").val(); 
 	var endDate = $("#endYear").val();
 	var recordsLimit = $("#numRecords").val();
 
 	searchURL += "?api-key=" + key + "&q=" + keyword;
 	if (startDate.length > 1){
-		searchURL += "&begin_date=" + startDate;
+		searchURL += "&begin_date=" + startDate + "0101";
 	}
 	if (endDate.length > 1){
-		searchURL += "&end_date=" + endDate;
+		searchURL += "&end_date=" + endDate + "1231";
 	}
-	//console.log(searchURL);
 		
 	$.ajax({
 	  url: searchURL,
@@ -32,8 +39,10 @@ function onSubmit(){
 	  		if (result.response.docs.length < recordsLimit){
 	  			loops = result.response.docs.length;
 	  		}
-
+			$("#results").empty();
+			
 		    for (var i = 0; i < loops; i++){
+				console.log(i);
 			    var articleNum = "article" + i;
 				
 				var article = $("<div>");	
@@ -44,9 +53,14 @@ function onSubmit(){
 
 			    var headline = "<h3 class='title'>" + currentArticle.headline.main + "</h3>";
 			    var articleSection = "<div class='section'" + currentArticle.section_name + "</div>";
-				var byline = "<div class='byline'>" + currentArticle.byline.original + "</div>";
-				var publishDate = "<div class='date'>" + currentArticle.pub_date + "</div>";
-			    var articleURL = "<div class='articleUrl'>" + currentArticle.web_url + "</div>";
+				var byline = "<div class='byline'>";
+				if(currentArticle.byline){
+					//sometimes this is null, and it throws an error
+					byline = currentArticle.byline.original;
+				}
+				byline += "</div>";
+				var publishDate = "<div class='date'>" + currentArticle.pub_date.split("T").shift() + "</div>";
+			    var articleURL = "<div class='articleUrl'><a href='" + currentArticle.web_url + "' target='_blank'>View Article >></a></div>";
 			    
 				article.append(headline).append(byline).append(publishDate).append(articleSection).append(articleURL);
 				$("#results").append(article);
